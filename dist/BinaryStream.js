b71a9f711f82e8e3d7ee8bed88056415292acb11
Filiph Sandström <filiph.sandstrom@filfatstudios.com>
2020-10-10 16:40:26 +0200

    Remove dist.

--- dist/BinaryStream.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class BinaryStream {
-    constructor(buffer = Buffer.alloc(0), offset = 0) {
-        this.buffer = buffer;
-        this.offset = offset;
-    }
-}
-exports.default = BinaryStream;
