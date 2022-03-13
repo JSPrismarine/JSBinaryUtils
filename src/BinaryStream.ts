d11126d33967127dedfafbbe9c14e2cb86797be8
Enrico Angelon <enricoangelon.ea@gmail.com>
2022-03-13 17:52:15 +0100

    docs(jsdoc): properly fix parameter types

--- src/BinaryStream.ts
+++ src/BinaryStream.ts
@@ -6,6 +6,11 @@ export default class BinaryStream {
     private readIndex: number;
     private writeIndex = 0;
 
+    /**
+     * Creates a new BinaryStream instance.
+     * @param {Buffer|null|undefined} buffer - The array or Buffer containing binary data.
+     * @param {number} offset - The initial pointer position.
+     */
     public constructor(buffer?: Buffer, offset: number = 0) {
         this.buffer = buffer ?? null; // Keep this instance for reading
         this.readIndex = offset;
@@ -13,7 +18,7 @@ export default class BinaryStream {
 
     /**
      * Reads a slice of buffer by the given length.
-     * @param len
+     * @param {number} len
      */
     public read(len: number): Buffer {
         this.doReadAssertions(len);
@@ -22,8 +27,7 @@ export default class BinaryStream {
 
     /**
      * Appends a buffer to the main buffer.
-     *
-     * @param buf
+     * @param {Buffer|Uint8Array} buf
      */
     public write(buf: Uint8Array): void;
     public write(buf: Buffer): void {
@@ -33,6 +37,7 @@ export default class BinaryStream {
 
     /**
      * Reads an unsigned byte (0 to 255).
+     * @returns {number}
      */
     public readByte(): number {
         this.doReadAssertions(1);
@@ -41,7 +46,7 @@ export default class BinaryStream {
 
     /**
      * Writes an unsigned byte (0 to 255).
-     * @param v
+     * @param {number} v
      */
     public writeByte(v: number): void {
         v &= 0xff;
@@ -59,7 +64,7 @@ export default class BinaryStream {
 
     /**
      * Writes a signed byte (-128 to 127).
-     * @param v
+     * @param {number} v
      */
     public writeSignedByte(v: number): void {
         if (v < 0) v = 0xff + v + 1;
@@ -77,7 +82,7 @@ export default class BinaryStream {
 
     /**
      * Writes a boolean (true or false).
-     * @param v
+     * @param {boolean} v
      */
     public writeBoolean(v: boolean): void {
         this.writeByte(+v);
@@ -94,7 +99,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 16 bit (2 bytes) signed big-endian number.
-     * @param v
+     * @param {number} v
      */
     public writeShort(v: number): void {
         this.doWriteAssertions(v, -32_768, 32_767);
@@ -113,7 +118,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 16 bit (2 bytes) signed big-endian number.
-     * @param v
+     * @param {number} v
      */
     public writeShortLE(v: number): void {
         this.doWriteAssertions(v, -32_768, 32_767);
@@ -132,7 +137,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 16 bit (2 bytes) unsigned big-endian number.
-     * @param v
+     * @param {number} v
      */
     public writeUnsignedShort(v: number): void {
         this.doWriteAssertions(v, 0, 65_535);
@@ -151,7 +156,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 16 bit (2 bytes) unsigned little-endian number.
-     * @param v
+     * @param {number} v
      */
     public writeUnsignedShortLE(v: number): void {
         this.doWriteAssertions(v, 0, 65_535);
@@ -170,7 +175,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 24 bit (3 bytes) signed big-endian number.
-     * @param v
+     * @param {number} v
      */
     public writeTriad(v: number): void {
         this.doWriteAssertions(v, -8_388_608, 8_388_607);
@@ -190,7 +195,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 24 bit (3 bytes) signed little-endian number.
-     * @param v
+     * @param {number} v
      */
     public writeTriadLE(v: number): void {
         this.doWriteAssertions(v, -8_388_608, 8_388_607);
@@ -210,7 +215,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 24 bit (3 bytes) unsigned big-endian number.
-     * @param v
+     * @param {number} v
      */
     public writeUnsignedTriad(v: number): void {
         this.doWriteAssertions(v, 0, 16_777_215);
@@ -230,7 +235,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 24 bit (3 bytes) unsigned little-endian number.
-     * @param v
+     * @param {number} v
      */
     public writeUnsignedTriadLE(v: number): void {
         this.doWriteAssertions(v, 0, 16_777_215);
@@ -241,6 +246,7 @@ export default class BinaryStream {
 
     /**
      * Reads a 32 bit (4 bytes) big-endian signed number.
+     * @returns {number}
      */
     public readInt(): number {
         this.doReadAssertions(4);
@@ -249,7 +255,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 32 bit (4 bytes) big-endian signed number.
-     * @param v
+     * @param {number} v
      */
     public writeInt(v: number): void {
         if (v < 0) v = v & (0xffffffff + v + 1);
@@ -262,6 +268,7 @@ export default class BinaryStream {
 
     /**
      * Reads a 32 bit (4 bytes) signed number.
+     * @returns {number}
      */
     public readIntLE(): number {
         this.doReadAssertions(4);
@@ -270,7 +277,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 32 bit (4 bytes) little-endian signed number.
-     * @param v
+     * @param {number} v
      */
     public writeIntLE(v: number) {
         if (v < 0) v = v & (0xffffffff + v + 1);
@@ -292,7 +299,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 32 bit (4 bytes) big-endian unsigned number.
-     * @param v
+     * @param {number} v
      */
     public writeUnsignedInt(v: number): void {
         this.doWriteAssertions(v, 0, 4_294_967_295);
@@ -313,7 +320,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 32 bit (4 bytes) little-endian unsigned number.
-     * @param v
+     * @param {number} v
      */
     public writeUnsignedIntLE(v: number): void {
         this.doWriteAssertions(v, 0, 4_294_967_295);
@@ -334,7 +341,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 32 bit (4 bytes) big-endian floating point number.
-     * @param v
+     * @param {number} v
      */
     public writeFloat(v: number): void {
         this.doWriteAssertions(
@@ -356,7 +363,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 32 bit (4 bytes) little-endian floating point number.
-     * @param v
+     * @param {number} v
      */
     public writeFloatLE(v: number): void {
         this.doWriteAssertions(
@@ -378,7 +385,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 64 bit (8 bytes) big-endian floating point number.
-     * @param v
+     * @param {number} v
      */
     public writeDouble(v: number): void {
         this.doWriteAssertions(
@@ -400,7 +407,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 64 bit (8 bytes) little-endian floating point number.
-     * @param v
+     * @param {number} v
      */
     public writeDoubleLE(v: number): void {
         this.doWriteAssertions(
@@ -422,7 +429,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 64 bit (8 bytes) signed big-endian number.
-     * @param v
+     * @param {bigint} v
      */
     public writeLong(v: bigint): void {
         const lo = Number(v & BigInt(0xffffffff));
@@ -449,7 +456,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 64 bit (8 bytes) signed big-endian number.
-     * @param v
+     * @param {bigint} v
      */
     public writeLongLE(v: bigint): void {
         const lo = Number(v & BigInt(0xffffffff));
@@ -475,7 +482,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 64 bit (8 bytes) unsigned big-endian number.
-     * @param v
+     * @param {bigint} v
      */
     public writeUnsignedLong(v: bigint): void {
         const lo = Number(v & BigInt(0xffffffff));
@@ -502,7 +509,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 64 bit (8 bytes) unsigned big-endian number.
-     * @param v
+     * @param {bigint} v
      */
     public writeUnsignedLongLE(v: bigint): void {
         const lo = Number(v & BigInt(0xffffffff));
@@ -519,6 +526,7 @@ export default class BinaryStream {
 
     /**
      * Reads a 32 bit (4 bytes) zigzag-encoded number.
+     * @returns {number}
      */
     public readVarInt(): number {
         const raw = this.readUnsignedVarInt();
@@ -528,8 +536,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 32 bit (4 bytes) zigzag-encoded number.
-     *
-     * @param v
+     * @param {number} v
      */
     public writeVarInt(v: number): void {
         v = (v << 32) >> 32;
@@ -538,6 +545,7 @@ export default class BinaryStream {
 
     /**
      * Reads a 32 bit unsigned number.
+     * @returns {number}
      */
     public readUnsignedVarInt(): number {
         assert(this.buffer != null, 'Reading on empty buffer!');
@@ -559,8 +567,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 32 bit unsigned number with variable-length.
-     *
-     * @param v
+     * @param {number} v
      */
     public writeUnsignedVarInt(v: number): void {
         while ((v & 0xffffff80) !== 0) {
@@ -572,6 +579,7 @@ export default class BinaryStream {
 
     /**
      * Reads a 64 bit zigzag-encoded variable-length number.
+     * @returns {bigint}
      */
     public readVarLong(): bigint {
         const raw = this.readUnsignedVarLong();
@@ -580,7 +588,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 64 bit unsigned zigzag-encoded number.
-     * @param v
+     * @param {bigint} v
      */
     public writeVarLong(v: bigint) {
         return this.writeUnsignedVarLong((v << 1n) ^ (v >> 63n));
@@ -588,6 +596,7 @@ export default class BinaryStream {
 
     /**
      * Reads a 64 bit unsigned variable-length number.
+     * @returns {bigint}
      */
     public readUnsignedVarLong(): bigint {
         let value = 0n;
@@ -608,8 +617,7 @@ export default class BinaryStream {
 
     /**
      * Writes a 64 bit unsigned variable-length number.
-     *
-     * @param v
+     * @param {bigint} v
      */
     public writeUnsignedVarLong(v: bigint) {
         for (let i = 0; i < 10; ++i) {
@@ -625,7 +633,7 @@ export default class BinaryStream {
 
     /**
      * Increases the write offset by the given length.
-     * @param length
+     * @param {number} length
      */
     private addOffset(length: number): number {
         return (this.readIndex += length) - length;
@@ -633,6 +641,7 @@ export default class BinaryStream {
 
     /**
      * Returns whatever or not the read offset is at end of line.
+     * @returns {number}
      */
     public feof(): boolean {
         if (!this.buffer) throw new Error('Buffer is write only!');
@@ -641,6 +650,7 @@ export default class BinaryStream {
 
     /**
      * Reads the remaining bytes and returns the buffer slice.
+     * @returns {Buffer}
      */
     public readRemaining(): Buffer {
         if (!this.buffer) throw new Error('Buffer is write only!');
@@ -651,7 +661,7 @@ export default class BinaryStream {
 
     /**
      * Skips len bytes on the buffer.
-     * @param len
+     * @param {number} len
      */
     public skip(len: number): void {
         assert(Number.isInteger(len), 'Cannot skip a float amount of bytes');
@@ -684,7 +694,7 @@ export default class BinaryStream {
 
     /**
      * Do read assertions, check if the read buffer is null.
-     * @param byteLength
+     * @param {number} byteLength
      */
     private doReadAssertions(byteLength: number): void {
         assert(this.buffer !== null, 'Cannot read without buffer data!');
@@ -696,7 +706,9 @@ export default class BinaryStream {
 
     /**
      * Do read assertions, check if the read buffer is null.
-     * @param byteLength
+     * @param {number|bigint} num
+     * @param {number|bigint} minVal
+     * @param {number|bigint} maxVal
      */
     private doWriteAssertions(
         num: number | bigint,
