9e810d99113ba9b079a89fb072ff41704bf29477
Enrico Angelon <enrico.angelon@officegroup.it>
2025-10-16 15:22:34 +0200

    fix: update Jest config for CI compatibility

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
-let ignore = ['<rootDir>/node_modules/'];
-
 module.exports = {
     testEnvironment: 'node',
     preset: 'ts-jest',
-    moduleFileExtensions: ['ts', 'js'],
-    reporters: ['default'],
-    testLocationInResults: true,
-    testMatch: ['**/*.(test|spec).(ts|js)'],
-    modulePathIgnorePatterns: ignore,
+    roots: ['<rootDir>/src'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+    collectCoverageFrom: ['src/**/*.ts', '!src/**/*.test.ts'],
     coverageReporters: ['json', 'lcov', 'text', 'text-summary']
 };
